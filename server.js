8f885cdc2d04f3847a3376022e4e897c8d046505
suganthan <suganthanvadivel@gmail.com>
2025-09-20 16:46:41 +0530

    DEPLOYMENT CHANGES 2

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend.vercel.app"
+      "https://workvibe-frontend.vercel.app/"
     ],
     credentials: true,
   })
